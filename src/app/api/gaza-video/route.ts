0bede7ab3833bd60862d9c77e910f8782afc986a
sheiikh-riiyad <riiyad.san1819@gmail.com>
2025-07-31 16:28:59 +0600

    fix  unknown error

--- src/app/api/gaza-video/route.ts
+++ src/app/api/gaza-video/route.ts
@@ -43,6 +43,6 @@ export async function GET() {
       thumbnail,
     });
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+   return NextResponse.json({ error: (error as Error).message }, { status: 500 });
   }
 }
